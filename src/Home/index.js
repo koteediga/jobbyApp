c0a47d2ae4eba61834181aa4fac2f823ae6f3606
koteswarrao <kotediga85@gmail.com>
2024-10-25 15:57:40 +0000

    submission change

--- src/Home/index.js
+++ src/Home/index.js
@@ -1,17 +1,32 @@
 import {Link} from 'react-router-dom'
+import Cookies from 'js-cookie'
 import Header from '../Header'
+
 import './index.css'
 
 const Home = props => {
   const findJobs = () => {}
+  const onClickLogout = () => {
+    const {history} = props
+
+    Cookies.remove('jwt_token')
+    history.replace('/login')
+  }
   return (
     <>
       <div>
         <Header />
+        <button
+          type="button"
+          className="logout-desktop-btn"
+          onClick={onClickLogout}
+        >
+          Logout
+        </button>
       </div>
       <div className="main-container">
         <h1>Find The Job That Fits Your Life</h1>
-        <p>Millions of People Searching for Job</p>
+        <p>Millions of people are searching for jobs</p>
         <Link to="/jobs">
           <button>Find Jobs</button>
         </Link>
