f9dd70c06f837b642ac78e857889426810e45cb7
koteswarrao <kotediga85@gmail.com>
2024-10-30 17:40:45 +0000

    job debscription route

--- src/jobDescription/index.js
+++ src/jobDescription/index.js
@@ -7,6 +7,7 @@ class jobDescription extends Component {
     companyDetails: {},
     similar: [],
     Loading: true,
+    isActive: false,
   }
 
   componentDidMount() {
@@ -33,12 +34,17 @@ class jobDescription extends Component {
       try {
         const response = await fetch(url, options)
         const data = await response.json()
-        console.log(data)
-        this.setState({
-          companyDetails: data.job_details,
-          similar: data.similar_jobs,
-          Loading: false,
-        })
+        if (response.ok) {
+          this.setState({
+            companyDetails: data.job_details,
+            similar: data.similar_jobs,
+            Loading: false,
+          })
+        } else {
+          this.setState({
+            isActive: true,
+          })
+        }
       } catch (error) {
         console.log(error)
       }
@@ -46,7 +52,7 @@ class jobDescription extends Component {
   }
 
   render() {
-    const {companyDetails, similar, Loading} = this.state
+    const {companyDetails, similar, Loading, isActive} = this.state
     const updatedata = {
       logo: companyDetails.company_logo_url,
       url1: companyDetails.company_website_url,
@@ -60,41 +66,64 @@ class jobDescription extends Component {
       rating: companyDetails.rating,
     }
     if (Loading) {
-      return <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+      return (
+        <div data-testid="loader">
+          <Loader type="ThreeDots" color="#0b69ff" height="50" width="50" />
+        </div>
+      )
     }
-
+    if (isActive) {
+      return (
+        <div>
+          <img
+            src="https://assets.ccbp.in/frontend/react-js/failure-img.png"
+            alt="failure view"
+          />
+          <h1>Oops! Something Went Wrong</h1>
+          <p>We cannot seem to find the page you are looking for</p>
+          <button>Retry</button>
+        </div>
+      )
+    }
     return (
       <div>
         <div>
-          <img src={updatedata.logo} alt="logo" />
-          <a href={updatedata.url1}>url</a>
-          <h1>{updatedata.type}</h1>
+          <img src={updatedata.logo} alt="job details company logo" />
+          <a href={updatedata.url1}>Visit</a>
+          <p>{updatedata.type}</p>
+          <h1>Description</h1>
+          <p>{updatedata.rating}</p>
           <p>{updatedata.description}</p>
           <h1>Skills</h1>
           <ul>
             {updatedata.skills.map(each => (
               <li>
-                <img src={each.image_url} />
+                <img src={each.image_url} alt="skills" />
                 <h1>{each.name}</h1>
               </li>
             ))}
           </ul>
           <h1>Life at Company</h1>
           <p>{updatedata.lifeAtCompany.description}</p>
-          <img src={updatedata.lifeAtCompany.image_url} />
+          <img src={updatedata.lifeAtCompany.image_url} alt="life_at_company" />
           <p>{updatedata.location}</p>
           <h1>{updatedata.rating}</h1>
-          <h1>{updatedata.package}</h1>
+          <p>{updatedata.package}</p>
         </div>
         <div>
+          <h1>Similar Jobs</h1>
           <ul>
             {similar.map(each => (
               <li>
-                <img src={each.company_logo_url} />
-                <h1>{each.employment_type}</h1>
+                <img
+                  src={each.company_logo_url}
+                  alt="similar job company logo"
+                />
+                <p>{each.employment_type}</p>
+                <h1>Description</h1>
                 <p>{each.description}</p>
                 <p>{each.location}</p>
-                <h1>{each.rating}</h1>
+                <p>{each.rating}</p>
                 <h1>{each.title}</h1>
               </li>
             ))}
